472a036d51dccb1dfe5c676f93b43f1ba0e1d767
Inayat Hussain <inayat>
2020-06-17 18:39:54 +0530

    fix

--- index.php
+++ index.php
@@ -40,7 +40,6 @@ The above copyright notice and this permission notice shall be included in all c
         <div class="container-fluid">
 		
 		 <div class="navbar-wrapper">
-            <a class="navbar-brand" href="javascript:;">Dashboard</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -48,17 +47,6 @@ The above copyright notice and this permission notice shall be included in all c
             <span class="navbar-toggler-icon icon-bar"></span>
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end">
-            <form class="navbar-form">
-              <span class="bmd-form-group"><div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                  <i class="material-icons">search</i>
-                  <div class="ripple-container"></div>
-                </button>
-              </div></span>
-            </form>
-			
           <div class="collapse navbar-collapse justify-content-end">
            
             <ul class="navbar-nav">
